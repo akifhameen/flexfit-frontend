8a9e8da9922c89de86b0d8cd4d8fe931e6dfdf2f
unknown <fathimasafaakbar@gmail.com>
2023-11-04 08:59:49 +0530

    comment removed

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -3,53 +3,6 @@ import './signIn.css';
 import weights from '../assets/images/weights.png';
 
 const SignIn = () => {
-  //     const[error ,setError] = useState('');
-  //     const[userInfo , setUserInfo] = useState({
-  //         email:'',
-  //         password:'',
-  //     })
-
-  //     const handleOnChangeText =(value, fieldName) =>{
-  //         setUserInfo({...userInfo,[fieldName]:value})
-  //     }
-
-  //     const isValidObjectField = (obj) =>{
-  //         return Object.values(obj).every(value => value.trim())
-
-  //     }
-
-  //       const updateError = (error,stateUpdater)=>{
-  //           stateUpdater(error);
-  //           setTimeout(()=>{
-  //               stateUpdater('')
-  //           },2500);
-  //       }
-
-  //       const isValidEmail = (value) =>{
-  //           const regx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
-  //           return regx.test(value)
-  //       }
-
-  //     const isValidForm =()=>{
-  //       // check all values
-  //       if(!isValidObjectField(userInfo)) return updateError('Required all fields',setError)
-  //       // only valid email is is allowed
-  //       if(!isValidEmail(email)) return updateError('Invalid email !', setError)
-  //       // password must be 8 characters
-  //       if(!password.trim() || password.length < 8) return updateError('Password is less than 8 characters', setError)
-
-  //       return true;
-  //   }
-  //     const submitForm =() =>{
-  //         if(isValidForm){
-  //             console.log('ggg')
-  //         }
-  //         else{
-  //             console.log('failed')
-  //         }
-  //     }
-
-  //     const {email,password} = userInfo
   return (
     <div class='signin__main-div'>
       <div class='signin__form-div'>
